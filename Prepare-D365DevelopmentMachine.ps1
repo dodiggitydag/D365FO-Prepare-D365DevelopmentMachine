098209cd6c296a82e485fcbdb29e076c257ea2e6
juliomutley <913772+juliomutley@users.noreply.github.com>
2020-11-17 12:47:48 +0100

    adjust formating

--- Prepare-D365DevelopmentMachine.ps1
+++ Prepare-D365DevelopmentMachine.ps1
@@ -28,8 +28,7 @@ $repo = "TrudAX/TRUDUtilsD365"
 $releases = "https://api.github.com/repos/$repo/releases"
 $path = "C:\Temp\Addin"
 
-If(!(test-path $path))
-{
+If (!(test-path $path)) {
     New-Item -ItemType Directory -Force -Path $path
 }
 cd $path
@@ -38,11 +37,10 @@ Write-Host Determining latest release
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 $tag = (Invoke-WebRequest -Uri $releases -UseBasicParsing | ConvertFrom-Json)[0].tag_name
 
-$files = @("InstallToVS.exe",  "TRUDUtilsD365.dll",  "TRUDUtilsD365.pdb")
+$files = @("InstallToVS.exe", "TRUDUtilsD365.dll", "TRUDUtilsD365.pdb")
 
 Write-Host Downloading files
-foreach ($file in $files) 
-{
+foreach ($file in $files) {
     $download = "https://github.com/$repo/releases/download/$tag/$file"
     [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
     Invoke-WebRequest $download -Out $file
